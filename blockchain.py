76eaa5331be6f9196aae9d967d2f5379106c5462
qinyunchuan <yunchuanqin@qq.com>
2018-07-08 18:55:41 +0800

    migrate blockchain into background

--- blockchain.py
+++ blockchain.py
@@ -230,49 +230,63 @@ def backgroundProcess(command,result):
             }
             result.put(response)
             print("haha")
+        elif comm['command'] == "/mine":
+            # We run the proof of work algorithm to get the next proof...
+            last_block = blockchain.last_block
+            proof = blockchain.proof_of_work(last_block)
+
+            # We must receive a reward for finding the proof.
+            # The sender is "0" to signify that this node has mined a new coin.
+            blockchain.new_transaction(
+                sender="0",
+                recipient=node_identifier,
+                amount=1,
+            )
+
+            # Forge the new Block by adding it to the chain
+            previous_hash = blockchain.hash(last_block)
+            block = blockchain.new_block(proof, previous_hash)
+
+            response = {
+                'message': "New Block Forged",
+                'index': block['index'],
+                'transactions': block['transactions'],
+                'proof': block['proof'],
+                'previous_hash': block['previous_hash'],
+            }
+            result.put(response)
+            print("haha")
+        elif comm['command'] =="'/transactions/new":
+            values = comm['values']
+            # Check that the required fields are in the POST'ed data
+            required = ['sender', 'recipient', 'amount']
+            if not all(k in values for k in required):
+                return 'Missing values', 400
+
+            # Create a new Transaction
+            index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+
+            response = {'message': f'Transaction will be added to Block {index}'}
+            result.put(response)
+            print("haha")
+
 
 
 @app.route('/mine', methods=['GET'])
 def mine():
-    # We run the proof of work algorithm to get the next proof...
-    last_block = blockchain.last_block
-    proof = blockchain.proof_of_work(last_block)
-
-    # We must receive a reward for finding the proof.
-    # The sender is "0" to signify that this node has mined a new coin.
-    blockchain.new_transaction(
-        sender="0",
-        recipient=node_identifier,
-        amount=1,
-    )
-
-    # Forge the new Block by adding it to the chain
-    previous_hash = blockchain.hash(last_block)
-    block = blockchain.new_block(proof, previous_hash)
-
-    response = {
-        'message': "New Block Forged",
-        'index': block['index'],
-        'transactions': block['transactions'],
-        'proof': block['proof'],
-        'previous_hash': block['previous_hash'],
-    }
+    command = {'command': '/mine'}
+    commandqueue.put(command)
+    result.put(response)
     return jsonify(response), 200
 
 
 @app.route('/transactions/new', methods=['POST'])
 def new_transaction():
     values = request.get_json()
+    command = {'command': '/nodes/register',
+               'values': values}
+    commandqueue.put(command)
 
-    # Check that the required fields are in the POST'ed data
-    required = ['sender', 'recipient', 'amount']
-    if not all(k in values for k in required):
-        return 'Missing values', 400
-
-    # Create a new Transaction
-    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
-
-    response = {'message': f'Transaction will be added to Block {index}'}
     return jsonify(response), 201
 
 
@@ -290,7 +304,6 @@ def register_nodes():
     command = {'command':'/nodes/register',
                'values':values}
     commandqueue.put(command)
-
     response = resultqueue.get()
     return jsonify(response), 201
 
